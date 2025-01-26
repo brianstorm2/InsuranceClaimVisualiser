faeda8a72cb024221c18412117d7deacad3b1e45
brianstorm2 <matt.j.parsons99@gmail.com>
2025-01-26 21:40:33 +0000

    Add Initial Code that generates a bar chart on claims by region

---
+++ InsuranceClaimVisualiser/DataVisualiser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System.Windows.Forms;
+
+namespace DataGraphProducer
+{
+    public class DataVisualiser
+    {
+        // Uses dictionary from ExtractClaimsByGeography method to generate a bar chart
+        public void GenerateClaimsBarChartByGeography(Dictionary<string, decimal> geographyData)
+        {
+            //initialise new bar chart
+            var plotModel = new PlotModel { Title = "Claims By Geography" };
+
+            //establish data points, labelled with geographical area
+            var barSeries = new BarSeries { LabelPlacement = LabelPlacement.Inside, LabelFormatString = "{0}" };
+
+            //foreach loop of dictionary contents entry = key, value pair
+            foreach (var entry in geographyData)
+            {
+                //add bar for each value
+                barSeries.Items.Add(new BarItem { Value = (double)entry.Value });
+            }
+
+            //adds bars to plotmodel
+            plotModel.Series.Add(barSeries);
+
+            //add labels to axis
+            var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
+            foreach (var entry in geographyData)
+            {
+                categoryAxis.Labels.Add(entry.Key);
+            }
+
+            //add labels to plotmodel
+            plotModel.Axes.Add(categoryAxis);
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = 0, Maximum = 2000 }); // Max value on axis
+
+            //creates view of bar chart
+            using (var plotView = new PlotView { Model = plotModel })
+            {
+                var form = new Form { Text = "Bar Chart", ClientSize = new System.Drawing.Size(800, 600) };
+                plotView.Dock = DockStyle.Fill;
+                form.Controls.Add(plotView);
+                form.ShowDialog();
+            }
+        }
+        //add lossratio visualiser by geography
+        //add claim visualiser by policy
+        //add loss ratio visualiser by policy
+        //add claim visualiser by month
+        //add loss ratio visualiser by month
+    }
+}
+
---
+++ InsuranceClaimVisualiser/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace LossRatioCalculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //use system environment variables for the program to retrieve username and password
+            string username = Environment.GetEnvironmentVariable("SQL_SERVER_USERNAME");
+            string password = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD");
+
+            //connect to local sql server db PolicyClaimsTrackerDB
+            string connectToLocalSqlServer = $"Server=localhost,1433;Database=PolicyClaimsTrackerDB; User Id={username}; Password={password};TrustServerCertificate=True";
+            using (SqlConnection connection = new SqlConnection(connectToLocalSqlServer))
+            {
+                connection.Open();
+                Console.WriteLine("Connection Opened");
+
+                //run ExtractClaimsByGeographyMethod
+                ExtractClaimsByGeography(connection);
+
+                connection.Close();
+            }
+
+        }
+
+        public static void ExtractClaimsByGeography(SqlConnection connection)
+        {
+            //query to group number of claims by geography
+            SqlCommand command = new SqlCommand("SELECT SUM(c.ClaimAmount) AS TotalClaims, r.RegionName FROM Claims c LEFT JOIN Policies p ON c.PolicyID = p.PolicyID LEFT JOIN Regions r ON r.RegionID = p.RegionID GROUP BY r.RegionName", connection);
+            Dictionary<string, decimal> claimsByRegion = new Dictionary<string, decimal>();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                //iterates through columns (0) = Column1
+                while (reader.Read())
+                {
+                    decimal totalClaims = reader.GetDecimal(0);
+                    string regionName = reader.GetString(1);
+
+                    //store results by geography claim in dictionary
+                    //move to seperate function
+                    claimsByRegion.Add(regionName, totalClaims);
+                    /*DateTime ClaimDate = reader.GetDateTime(3);
+                    Console.WriteLine($"ClaimID: {ClaimID}, PolicyID: {PolicyID}, Claim Amount: {ClaimAmount}, Claim Date: {ClaimDate.ToShortDateString()}");*/
+                }
+                foreach (KeyValuePair<string, decimal> kvp in claimsByRegion)
+                {
+                    Console.WriteLine($"Region {kvp.Key}, Total Claims: {kvp.Value}");
+                }
+                //Generate Bar Chart
+                DataGraphProducer.DataVisualiser visualiseByGeography = new DataGraphProducer.DataVisualiser();
+                visualiseByGeography.GenerateClaimsBarChartByGeography(claimsByRegion);
+            }
+        }
+    }
+}
